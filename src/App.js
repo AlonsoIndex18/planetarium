ea85b740c2d24126db20f51341557620acf20a76
AlonsoIndex18 <alonso_avalo97@hotmail.com>
2021-11-09 19:02:21 -0500

    added space

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function createPathForStrings(filename){
   const fileType = '.png';
   const sides = ['1', '2', '3', '4', '5', '6'];
   const pathStings = sides.map(side => {
-    console.log(side);
+    console.log(baseFileName + '_' + side + fileType);
     return baseFileName + '_' + side + fileType;
   });
   return pathStings;
